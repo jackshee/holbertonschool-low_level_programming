13ba4b50f82c9669d9ac8d3956ecb23fc394a9c3
Jack Shee <jackshee97@gmail.com>
2025-10-20 23:30:22 +0000

    isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+/**
+ * _isdigit - function to check if c is a digit
+ * @c: integer input character
+ *
+ * Description: checks if the input is an ASCII digit 
+ * Return: 1 (if c is digit), 0 otherwise
+ *
+ */
+int _isdigit(int c)
+{
+	if (c >= "0" && c <= "9")
+		return (1);
+	return (0);
+}
+
