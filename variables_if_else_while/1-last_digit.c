471f599fe81d55cc025391303ef8c44b27904c3b
Jack Shee <jackshee97@gmail.com>
2025-10-20 22:50:11 +0000

    still not fixed?

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,26 +6,24 @@
 /**
  * main - Enter
  *
- * Return - 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int last_digit;
 	int n;
 	const char *output_str;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* use modulo 10 to get last digit (remainder) */ 
+	/* use modulo 10 to get last digit (remainder) */
 	last_digit = abs(n) % 10;
-
 	if (last_digit > 5)
 		output_str = " and is greater than 5";
 	else if (last_digit == 0)
 		output_str = " and is 0";
 	else
 		output_str = " and is less than 6 and not 0";
-		
-	printf("Last digit of %d is %d%s\n", n, last_digit, output_str); 
+	printf("Last digit of %d is %d%s\n", n, last_digit, output_str);
 	return (0);
 }
