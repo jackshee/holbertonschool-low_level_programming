682e92d6cceae562e69f664f88adc7337b9646e1
Jack Shee <jackshee97@gmail.com>
2025-10-20 23:37:55 +0000

    mult

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - multiplies two integers 
+ * @a: integer
+ * @b: integer
+ *
+ * Description: multiplication function
+ * Return: product of two integers (int)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
