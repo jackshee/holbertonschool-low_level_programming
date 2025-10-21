4a5e79bf343a3c367a19fb475092848c0dd507b0
Jack Shee <jackshee97@gmail.com>
2025-10-21 00:18:28 +0000

    reverse alpha

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: 0 (always success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
