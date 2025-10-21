d705832865b26917378da8456a806e729c25b1be
Jack Shee <jackshee97@gmail.com>
2025-10-21 00:16:14 +0000

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: 0 (always success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
